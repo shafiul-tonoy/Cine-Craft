296833da712624423ffeb3d0752aec5fae6d2ef1
MD. Shafiul Anam <tonoy.tech@gmail.com>
2024-12-08 05:05:31 +0600

    home added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,18 @@
+import { useLoaderData } from "react-router-dom";
+
 export default function Home(){
+    const movies = useLoaderData()
     return (
-        <div>
-            <h1>Home</h1>
+        <div className= 'w-full md:w-10/12 md:mx-auto p-5' >
+            {movies.map((movie) => (
+                <div key={movie._id}>
+                    <h2>{movie.title}</h2>
+                    <p>{movie.genre}</p>
+                    <p>{movie.duration}</p>
+                    <p>{movie.releaseYear}</p>
+                    <p>{movie.rating}</p>
+                </div>
+            ))}
         </div>
     );
 }
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -23,6 +23,7 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+        loader: () => fetch(`http://localhost:5000`),
       },
       {
         path: "allMovies",
